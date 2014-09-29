bde79f943a81411517dc977d8480fd6907272f6d
Rob Mensching <rob@robmensching.com>
2014-09-28 20:40:31 -0700

    Fix version warning in SettingsEngineTest project.

--- test/src/SettingsEngineTests/AssemblyInfo.cpp
+++ test/src/SettingsEngineTests/AssemblyInfo.cpp
@@ -17,18 +17,4 @@ using namespace System::Runtime::InteropServices;
 [assembly:AssemblyCopyrightAttribute("Copyright (c)  2010")];
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
-
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the value or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-
-[assembly:AssemblyVersionAttribute("1.0.*")];
 [assembly:ComVisible(false)];
-
