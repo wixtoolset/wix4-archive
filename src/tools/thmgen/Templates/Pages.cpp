b85da7ae7dd6a9179f6ed69f45307021f85bebef
Bob Arnson <bob@joyofsetup.com>
2015-12-18 00:52:20 -0500

    ThemeGen utility, which generates the control and page arrays to simplify control and page management for ThmUtil.

---
+++ src/tools/thmgen/Templates/Pages.cpp
@@ -0,0 +1,25 @@
+﻿static LPCWSTR vrgwzPageNames[] = {{
+{1}
+}};
+
+static DWORD vrgdwPageIds[countof(vrgwzPageNames)] = {{ }};
+static Pages vpageCurrent = Pages::PageCount;
+
+void Show{0}Page(
+    __in THEME* pTheme,
+    __in Pages page
+    )
+{{
+    if (Pages::PageCount != vpageCurrent)
+    {{
+        ThemeShowPage(pTheme, vrgdwPageIds[vpageCurrent], SW_HIDE);
+    }}
+
+    vpageCurrent = page;
+    ThemeShowPage(pTheme, vrgdwPageIds[page], SW_SHOW);
+}}
+
+Pages GetCurrent{0}Page()
+{{
+    return vpageCurrent;
+}}
