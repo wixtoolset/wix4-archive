wixtoolset/wix3

Have Burn rewrite ARP DisplayName based on WixBundleName variable during modify.

src/burn/engine/registration.cpp
@@ -98,7 +98,11 @@ static HRESULT RegWriteStringVariable(
     __in LPCWSTR wzVariable,
     __in LPCWSTR wzName
     );
-
+static HRESULT UpdateBundleNameRegistration(
+    __in BURN_REGISTRATION* pRegistration,
+    __in BURN_VARIABLES* pVariables,
+    __in HKEY hkRegistration
+    );
 
 // function definitions
 
@@ -600,7 +604,6 @@ extern "C" HRESULT RegistrationSessionBegin(
     HRESULT hr = S_OK;
     DWORD dwSize = 0;
     HKEY hkRegistration = NULL;
-    LPWSTR sczDisplayName = NULL;
     LPWSTR sczPublisher = NULL;
 
     LogId(REPORT_VERBOSE, MSG_SESSION_BEGIN, pRegistration->sczRegistrationKey, dwRegistrationOptions, LoggingBoolToString(pRegistration->fDisableResume));
@@ -661,10 +664,9 @@ extern "C" HRESULT RegistrationSessionBegin(
         hr = RegWriteStringFormatted(hkRegistration, REGISTRY_BUNDLE_DISPLAY_ICON, L"%s,0", pRegistration->sczCacheExecutablePath);
         ExitOnFailure(hr, "Failed to write %ls value.", REGISTRY_BUNDLE_DISPLAY_ICON);
 
-        // DisplayName: provided by UI
-        hr = GetBundleName(pRegistration, pVariables, &sczDisplayName);
-        hr = RegWriteString(hkRegistration, BURN_REGISTRATION_REGISTRY_BUNDLE_DISPLAY_NAME, SUCCEEDED(hr) ? sczDisplayName : pRegistration->sczDisplayName);
-        ExitOnFailure(hr, "Failed to write %ls value.", BURN_REGISTRATION_REGISTRY_BUNDLE_DISPLAY_NAME);
+        // update display name
+        hr = UpdateBundleNameRegistration(pRegistration, pVariables, hkRegistration);
+        ExitOnFailure(hr, "Failed to update name and publisher.");
 
         // DisplayVersion: provided by UI
         if (pRegistration->sczDisplayVersion)
@@ -823,7 +825,6 @@ extern "C" HRESULT RegistrationSessionBegin(
     ExitOnFailure(hr, "Failed to update resume mode.");
 
 LExit:
-    ReleaseStr(sczDisplayName);
     ReleaseStr(sczPublisher);
     ReleaseRegKey(hkRegistration);
 
@@ -836,7 +837,8 @@ LExit:
 
 *******************************************************************/
 extern "C" HRESULT RegistrationSessionResume(
-    __in BURN_REGISTRATION* pRegistration
+    __in BURN_REGISTRATION* pRegistration,
+    __in BURN_VARIABLES* pVariables
     )
 {
     HRESULT hr = S_OK;
@@ -849,6 +851,10 @@ extern "C" HRESULT RegistrationSessionResume(
     // update resume mode
     hr = UpdateResumeMode(pRegistration, hkRegistration, BURN_RESUME_MODE_ACTIVE, FALSE);
     ExitOnFailure(hr, "Failed to update resume mode.");
+
+    // update display name
+    hr = UpdateBundleNameRegistration(pRegistration, pVariables, hkRegistration);
+    ExitOnFailure(hr, "Failed to update name and publisher.");
 
 LExit:
     ReleaseRegKey(hkRegistration);
@@ -1576,6 +1582,26 @@ static HRESULT RegWriteStringVariable(
 
 LExit:
     StrSecureZeroFreeString(sczValue);
+
+    return hr;
+}
+
+static HRESULT UpdateBundleNameRegistration(
+    __in BURN_REGISTRATION* pRegistration,
+    __in BURN_VARIABLES* pVariables,
+    __in HKEY hkRegistration
+    )
+{
+    HRESULT hr = S_OK;
+    LPWSTR sczDisplayName = NULL;
+
+    // DisplayName: provided by UI
+    hr = GetBundleName(pRegistration, pVariables, &sczDisplayName);
+    hr = RegWriteString(hkRegistration, BURN_REGISTRATION_REGISTRY_BUNDLE_DISPLAY_NAME, SUCCEEDED(hr) ? sczDisplayName : pRegistration->sczDisplayName);
+    ExitOnFailure1(hr, "Failed to write %ls value.", BURN_REGISTRATION_REGISTRY_BUNDLE_DISPLAY_NAME);
+
+LExit:
+    ReleaseStr(sczDisplayName);
 
     return hr;
 }

wixtoolset/wix4-archive

src/burn/engine/registration.cpp
@@ -96,7 +96,11 @@ static HRESULT RegWriteStringVariable(
     __in LPCWSTR wzVariable,
     __in LPCWSTR wzName
     );
-
+static HRESULT UpdateBundleNameRegistration(
+    __in BURN_REGISTRATION* pRegistration,
+    __in BURN_VARIABLES* pVariables,
+    __in HKEY hkRegistration
+    );
 
 // function definitions
 
@@ -597,7 +601,6 @@ extern "C" HRESULT RegistrationSessionBegin(
     HRESULT hr = S_OK;
     DWORD dwSize = 0;
     HKEY hkRegistration = NULL;
-    LPWSTR sczDisplayName = NULL;
     LPWSTR sczPublisher = NULL;
 
     LogId(REPORT_VERBOSE, MSG_SESSION_BEGIN, pRegistration->sczRegistrationKey, dwRegistrationOptions, LoggingBoolToString(pRegistration->fDisableResume));
@@ -658,10 +661,9 @@ extern "C" HRESULT RegistrationSessionBegin(
         hr = RegWriteStringFormatted(hkRegistration, REGISTRY_BUNDLE_DISPLAY_ICON, L"%s,0", pRegistration->sczCacheExecutablePath);
         ExitOnFailure(hr, "Failed to write %ls value.", REGISTRY_BUNDLE_DISPLAY_ICON);
 
-        // DisplayName: provided by UI
-        hr = GetBundleName(pRegistration, pVariables, &sczDisplayName);
-        hr = RegWriteString(hkRegistration, BURN_REGISTRATION_REGISTRY_BUNDLE_DISPLAY_NAME, SUCCEEDED(hr) ? sczDisplayName : pRegistration->sczDisplayName);
-        ExitOnFailure(hr, "Failed to write %ls value.", BURN_REGISTRATION_REGISTRY_BUNDLE_DISPLAY_NAME);
+        // update display name
+        hr = UpdateBundleNameRegistration(pRegistration, pVariables, hkRegistration);
+        ExitOnFailure(hr, "Failed to update name and publisher.");
 
         // DisplayVersion: provided by UI
         if (pRegistration->sczDisplayVersion)
@@ -820,7 +822,6 @@ extern "C" HRESULT RegistrationSessionBegin(
     ExitOnFailure(hr, "Failed to update resume mode.");
 
 LExit:
-    ReleaseStr(sczDisplayName);
     ReleaseStr(sczPublisher);
     ReleaseRegKey(hkRegistration);
 
@@ -833,7 +834,8 @@ LExit:
 
 *******************************************************************/
 extern "C" HRESULT RegistrationSessionResume(
-    __in BURN_REGISTRATION* pRegistration
+    __in BURN_REGISTRATION* pRegistration,
+    __in BURN_VARIABLES* pVariables
     )
 {
     HRESULT hr = S_OK;
@@ -846,6 +848,10 @@ extern "C" HRESULT RegistrationSessionResume(
     // update resume mode
     hr = UpdateResumeMode(pRegistration, hkRegistration, BURN_RESUME_MODE_ACTIVE, FALSE);
     ExitOnFailure(hr, "Failed to update resume mode.");
+
+    // update display name
+    hr = UpdateBundleNameRegistration(pRegistration, pVariables, hkRegistration);
+    ExitOnFailure(hr, "Failed to update name and publisher.");
 
 LExit:
     ReleaseRegKey(hkRegistration);
@@ -1569,6 +1575,26 @@ static HRESULT RegWriteStringVariable(
 
 LExit:
     StrSecureZeroFreeString(sczValue);
+
+    return hr;
+}
+
+static HRESULT UpdateBundleNameRegistration(
+    __in BURN_REGISTRATION* pRegistration,
+    __in BURN_VARIABLES* pVariables,
+    __in HKEY hkRegistration
+    )
+{
+    HRESULT hr = S_OK;
+    LPWSTR sczDisplayName = NULL;
+
+    // DisplayName: provided by UI
+    hr = GetBundleName(pRegistration, pVariables, &sczDisplayName);
+    hr = RegWriteString(hkRegistration, BURN_REGISTRATION_REGISTRY_BUNDLE_DISPLAY_NAME, SUCCEEDED(hr) ? sczDisplayName : pRegistration->sczDisplayName);
+    ExitOnFailure(hr, "Failed to write %ls value.", BURN_REGISTRATION_REGISTRY_BUNDLE_DISPLAY_NAME);
+
+LExit:
+    ReleaseStr(sczDisplayName);
 
     return hr;
 }