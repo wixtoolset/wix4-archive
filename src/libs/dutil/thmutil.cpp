8f430da9e01960bd6335fd06fe732a836c568f2b
Bob Arnson <bob@firegiant.com>
2016-03-05 13:18:55 -0500

    Localize command link note text and add ThmGen to the build.

--- src/libs/dutil/thmutil.cpp
+++ src/libs/dutil/thmutil.cpp
@@ -4921,6 +4921,12 @@ static HRESULT LocalizeControl(
         }
     }
 
+    if (pControl->sczNote && *pControl->sczNote)
+    {
+        hr = LocLocalizeString(pWixLoc, &pControl->sczNote);
+        ExitOnFailure(hr, "Failed to localize control note text.");
+    }
+
     for (DWORD j = 0; j < pControl->cConditionalText; ++j)
     {
         hr = LocLocalizeString(pWixLoc, &pControl->rgConditionalText[j].sczText);
