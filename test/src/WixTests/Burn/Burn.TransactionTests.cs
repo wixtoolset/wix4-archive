56fc7595f4fc69c198d167dda464c226c9f45dc8
nir.bar <nir.bar@panel-sw.com>
2016-09-24 22:02:36 +0300

    Thicken transaction rollback test: Rollback should rollback a package that performed a major upgrade. The preceding package (the one that was upgraded from) should be present after the transaction rolled back.

--- test/src/WixTests/Burn/Burn.TransactionTests.cs
+++ test/src/WixTests/Burn/Burn.TransactionTests.cs
@@ -59,6 +59,14 @@ namespace WixTest.Tests.Burn
             this.Complete();
         }
 
+        /// <summary>
+        /// Installs 2 bundles:
+        ///   bundleC- installs package E
+        ///   bundleB- installes packages A, B, D
+        ///     package B performs a major upgrade of package E
+        ///     package D fails
+        ///     Thus, rolling back the transaction should reinstall package E
+        /// </summary>
         [NamedFact]
         [Priority(2)]
         [Description("Installs a bundle with an MSI transaction that should fail.")]
@@ -69,32 +77,51 @@ namespace WixTest.Tests.Burn
             string packageA = new PackageBuilder(this, "A").Build().Output;
             string packageB = new PackageBuilder(this, "B").Build().Output;
             string packageD = new PackageBuilder(this, "D").Build().Output;
+            string packageE = new PackageBuilder(this, "E").Build().Output;
 
             // Create the named bind paths to the packages.
-            Dictionary<string, string> bindPaths = new Dictionary<string, string>();
-            bindPaths.Add("packageA", Path.GetDirectoryName(packageA));
-            bindPaths.Add("packageB", Path.GetDirectoryName(packageB));
-            bindPaths.Add("packageD", Path.GetDirectoryName(packageD));
+            Dictionary<string, string> bundleBbindPaths = new Dictionary<string, string>();
+            bundleBbindPaths.Add("packageA", Path.GetDirectoryName(packageA));
+            bundleBbindPaths.Add("packageB", Path.GetDirectoryName(packageB));
+            bundleBbindPaths.Add("packageD", Path.GetDirectoryName(packageD));
 
-            string bundleB = new BundleBuilder(this, "BundleB") { BindPaths = bindPaths, Extensions = Extensions }.Build().Output;
+            Dictionary<string, string> bundleCbindPaths = new Dictionary<string, string>();
+            bundleCbindPaths.Add("packageE", Path.GetDirectoryName(packageE));
+
+            string bundleB = new BundleBuilder(this, "BundleB") { BindPaths = bundleBbindPaths, Extensions = Extensions }.Build().Output;
+            string bundleC = new BundleBuilder(this, "BundleC") { BindPaths = bundleCbindPaths, Extensions = Extensions }.Build().Output;
 
             string packageASourceCodeInstalled = this.GetTestInstallFolder(@"A\A.wxs");
             string packageBSourceCodeInstalled = this.GetTestInstallFolder(@"B\B.wxs");
             string packageDSourceCodeInstalled = this.GetTestInstallFolder(@"D\D.wxs");
+            string packageESourceCodeInstalled = this.GetTestInstallFolder(@"E\E.wxs");
+
+            // Source file should *not* be installed
+            Assert.False(File.Exists(packageASourceCodeInstalled), String.Concat("Package A payload should not be there on test start: ", packageASourceCodeInstalled));
+            Assert.False(File.Exists(packageBSourceCodeInstalled), String.Concat("Package B payload should not be there on test start: ", packageBSourceCodeInstalled));
+            Assert.False(File.Exists(packageDSourceCodeInstalled), String.Concat("Package D payload should not be there on test start: ", packageDSourceCodeInstalled));
+            Assert.False(File.Exists(packageESourceCodeInstalled), String.Concat("Package E payload should not be there on test start: ", packageESourceCodeInstalled));
+
+            BundleInstaller installC = new BundleInstaller(this, bundleC).Install();
+
+            // Source file should be installed
+            Assert.True(File.Exists(packageESourceCodeInstalled), String.Concat("Should have found Package E payload installed at: ", packageESourceCodeInstalled));
 
             // Source file should *not* be installed
             Assert.False(File.Exists(packageASourceCodeInstalled), String.Concat("Package A payload should not be there on test start: ", packageASourceCodeInstalled));
             Assert.False(File.Exists(packageBSourceCodeInstalled), String.Concat("Package B payload should not be there on test start: ", packageBSourceCodeInstalled));
             Assert.False(File.Exists(packageDSourceCodeInstalled), String.Concat("Package D payload should not be there on test start: ", packageDSourceCodeInstalled));
 
-            BundleInstaller install = new BundleInstaller(this, bundleB).Install(1603);
-
+            // Bundle B should fail
+            BundleInstaller installB = new BundleInstaller(this, bundleB).Install(1603);
+           
             // Source file should be installed
             Assert.True(File.Exists(packageASourceCodeInstalled), String.Concat("Should have found Package A payload installed at: ", packageASourceCodeInstalled));
+            Assert.True(File.Exists(packageESourceCodeInstalled), String.Concat("Should have found Package E payload that survived a failed major upgrade installed at: ", packageESourceCodeInstalled));
 
             // Source file should *not* be installed
-            Assert.False(File.Exists(packageBSourceCodeInstalled), String.Concat("Package B payload should not have been installed to: ", packageBSourceCodeInstalled));
-            Assert.False(File.Exists(packageDSourceCodeInstalled), String.Concat("Package D payload should not have been installed to: ", packageDSourceCodeInstalled));
+            Assert.False(File.Exists(packageBSourceCodeInstalled), String.Concat("Package B payload should not be there on test start: ", packageBSourceCodeInstalled));
+            Assert.False(File.Exists(packageDSourceCodeInstalled), String.Concat("Package D payload should not be there on test start: ", packageDSourceCodeInstalled));
 
             this.Complete();
         }
